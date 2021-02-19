021c6db740ea90ed7a1600267c80fc1ce9ae573d
rochellev <rochellevroberts@utexas.edu>
2021-02-19 15:49:23 -0800

    add survey component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
+import Survey from './components/Survey';
 
 const App = () => {
   return(
-    <div>
-      app
+    <div className="ui container comments">
+      <h1 className="ui header">Sir Survey</h1>
+      <Survey />
     </div>
   )
 }
